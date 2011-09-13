85bada6af19116b51df6956b941358423526e9c5
Quentin Glidic <sardemff7+git@sardemff7.net>
2011-09-13 23:23:31 +0200

    Fix headers

--- src/eventd/service.c
+++ src/eventd/service.c
@@ -1,20 +1,23 @@
 /*
  * eventd - Small daemon to act on remote or local events
  *
- * Copyright © 2011 Sardem FF7
+ * Copyright © 2011 Quentin "Sardem FF7" Glidic
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of eventd.
+ *
+ * eventd is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * eventd is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with eventd. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include "eventd.h"
