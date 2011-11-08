a179abfcf3d7f0abd0f0fc82e3352027f45fc1ea
Quentin Glidic <sardemff7+git@sardemff7.net>
2011-11-08 14:26:50 +0100

    eventd: Load configuration from datadir as well

--- src/eventd/config.c
+++ src/eventd/config.c
@@ -184,6 +184,7 @@ eventd_config_parser()
 
     config = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 
+    eventd_config_load_dir(DATADIR);
     eventd_config_load_dir(SYSCONFDIR);
     eventd_config_load_dir(g_get_user_config_dir());
 }
