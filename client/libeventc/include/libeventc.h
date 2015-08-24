5874ee06c2cd3700500a65bb6b6517bda7fcfe78
Quentin Glidic <sardemff7+git@sardemff7.net>
2015-08-24 13:37:25 +0200

    libeventc: Relicence to LGPLv3
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- client/libeventc/include/libeventc.h
+++ client/libeventc/include/libeventc.h
@@ -1,22 +1,20 @@
 /*
-  *libeventc - Library to communicate with eventd
+ * libeventc - Library to communicate with eventd
  *
-  *Copyright © 2011-2012 Quentin "Sardem FF7" Glidic
+ * Copyright © 2011-2012 Quentin "Sardem FF7" Glidic
  *
-  *This file is part of eventd.
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
-  *eventd is free software: you can redistribute it and/or modify
-  *it under the terms of the GNU General Public License as published by
-  *the Free Software Foundation, either version 3 of the License, or
-  *(at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  *
-  *eventd is distributed in the hope that it will be useful,
-  *but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  *GNU General Public License for more details.
- *
-  *You should have received a copy of the GNU General Public License
-  *along with eventd. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with eventd. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
