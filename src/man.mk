90be129fc1c2848aa6f9e31a2217b8dcf0134a4c
Quentin Glidic <sardemff7+git@sardemff7.net>
2015-09-20 12:06:59 +0200

    man: Remove leftover *dir substitutions
    There are only direct substitutions left, so configure will do the job
    just right
    
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- src/man.mk
+++ src/man.mk
@@ -1,12 +1,10 @@
 EXTRA_DIST += \
-	%D%/config.ent.in \
 	%D%/common-man.xml \
 	$(man1_MANS:.1=.xml) \
 	$(man5_MANS:.5=.xml) \
 	$(null)
 
 CLEANFILES += \
-	%D%/config.ent \
 	$(man1_MANS) \
 	$(man5_MANS) \
 	$(null)
@@ -29,17 +27,3 @@ $(man1_MANS): %.1: %.xml $(NKUTILS_MANFILES) %D%/common-man.xml %D%/config.ent
 
 $(man5_MANS): %.5: %.xml $(NKUTILS_MANFILES) %D%/common-man.xml %D%/config.ent
 	$(MAN_GEN_RULE)
-
-%D%/config.ent: %D%/config.ent.in $(CONFIG_HEADER) %D%/man.mk
-	$(EV_V_MAN)$(MKDIR_P) $(dir $@) && \
-		$(SED) \
-		-e 's:[@]sysconfdir[@]:$(sysconfdir):g' \
-		-e 's:[@]libdir[@]:$(libdir):g' \
-		-e 's:[@]datadir[@]:$(datadir):g' \
-		-e 's:[@]PACKAGE_NAME[@]:$(PACKAGE_NAME):g' \
-		-e 's:[@]PACKAGE_VERSION[@]:$(PACKAGE_VERSION):g' \
-		-e 's:[@]EVP_SERVICE_NAME[@]:$(EVP_SERVICE_NAME):g' \
-		-e 's:[@]EVP_TRANSPORT_NAME[@]:$(EVP_TRANSPORT_NAME):g' \
-		-e 's:[@]EVP_SERVICE_TYPE[@]:$(EVP_SERVICE_TYPE):g' \
-		-e 's:[@]EVP_UNIX_SOCKET[@]:$(EVP_UNIX_SOCKET):g' \
-		< $< > $@ || rm $@
