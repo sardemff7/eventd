612bed012d2c246bcd15d58545b73e043ad2004d
Quentin Glidic <sardemff7+git@sardemff7.net>
2015-10-27 10:14:59 +0100

    test-plugin: Drop unused eventd.conf
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- plugins/test-plugin/test-plugin.mk
+++ plugins/test-plugin/test-plugin.mk
@@ -4,26 +4,11 @@ check_LTLIBRARIES += \
 	test-plugin.la \
 	$(null)
 
-check_DATA += \
-	$(builddir)/%D%/config/eventd.conf \
-	$(builddir)/%D%/config/test.pem \
+dist_check_DATA += \
 	$(builddir)/%D%/config/test.event \
 	$(builddir)/%D%/config/test.action \
 	$(null)
 
-EXTRA_DIST += \
-	%D%/config/eventd.conf.in \
-	%D%/config/test.pem.in \
-	%D%/config/test.event.in \
-	%D%/config/test.action.in \
-	$(null)
-
-CLEANFILES += \
-	$(builddir)/%D%/config/eventd.conf \
-	$(builddir)/%D%/config/test.pem \
-	$(builddir)/%D%/config/test.event \
-	$(builddir)/%D%/config/test.action \
-	$(null)
 
 test_plugin_la_SOURCES = \
 	%D%/src/test-plugin.c \
@@ -45,13 +30,3 @@ test_plugin_la_LIBADD = \
 	libeventd-plugin.la \
 	$(GLIB_LIBS) \
 	$(null)
-
-$(builddir)/%D%/config/eventd.conf: $(srcdir)/%D%/config/eventd.conf.in Makefile
-	$(AM_V_GEN)$(MKDIR_P) $(dir $@) && \
-		$(SED) \
-		-e 's:[@]CONFIG_DIR[@]:$(abs_builddir)/%D%/config:g' \
-		< $< > $@ || rm $@
-
-$(builddir)/%D%/config/%: $(srcdir)/%D%/config/%.in
-	$(AM_V_GEN)$(MKDIR_P) $(dir $@) && \
-		cp $< $@
