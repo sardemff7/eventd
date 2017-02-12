0afcf9499552cedc1b348782c954ac89a5e00add
Quentin Glidic <sardemff7+git@sardemff7.net>
2017-02-12 12:56:59 +0100

    eventd/control,eventdctl: Use EVENTD_VERSION
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- server/eventdctl/src/eventdctl.c
+++ server/eventdctl/src/eventdctl.c
@@ -319,7 +319,7 @@ main(int argc, char *argv[])
 
     if ( print_version )
     {
-        fprintf(stdout, "eventdctl " PACKAGE_VERSION "\n");
+        g_printf(PACKAGE_NAME " " EVENTD_VERSION "\n");
         return EVENTDCTL_RETURN_CODE_OK;
     }
 
