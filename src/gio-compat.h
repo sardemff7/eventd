81e0216f120f90b98e25ba07ac3e48e0e752d8ed
Quentin Glidic <sardemff7+git@sardemff7.net>
2017-04-17 00:43:14 +0200

    gio-compat: Fix g_socket_connectable_to_string() fallback
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- src/gio-compat.h
+++ src/gio-compat.h
@@ -31,16 +31,16 @@ g_socket_connectable_to_string(GSocketConnectable *c)
 {
     if ( G_IS_UNIX_SOCKET_ADDRESS(c) )
         return g_strdup(g_unix_socket_address_get_path(G_UNIX_SOCKET_ADDRESS(c)));
-    if ( G_IS_NETWORK_SERVICE(server_identity) )
+    if ( G_IS_NETWORK_SERVICE(c) )
         return g_strdup(g_network_service_get_domain(G_NETWORK_SERVICE(c)));
     if ( G_IS_NETWORK_ADDRESS(c) )
         return g_strdup_printf("%s:%u", g_network_address_get_hostname(G_NETWORK_ADDRESS(c)),  g_network_address_get_port(G_NETWORK_ADDRESS(c)));
     if ( G_IS_PROXY_ADDRESS(c) )
-        return g_strdup_printf("%s:%u", g_proxy_address_get_destination_hostname(G_PROXY_ADDRESS(c)),  g_proxy_address_get_destination_port(G_PROXY_ADDRESS(c))
+        return g_strdup_printf("%s:%u", g_proxy_address_get_destination_hostname(G_PROXY_ADDRESS(c)),  g_proxy_address_get_destination_port(G_PROXY_ADDRESS(c)));
     if ( G_IS_INET_SOCKET_ADDRESS(c) )
     {
         GInetAddress *inet_address;
-        gchar *str, tmp;
+        gchar *str, *tmp;
         inet_address = g_inet_socket_address_get_address(G_INET_SOCKET_ADDRESS(c));
         tmp = g_inet_address_to_string(inet_address);
         str = g_strdup_printf("%s:%u", tmp,  g_inet_socket_address_get_port(G_INET_SOCKET_ADDRESS(c)));
