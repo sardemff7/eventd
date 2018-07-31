f296fb5c054d5eb2fd480eb36277690c408d962d
Quentin Glidic <sardemff7+git@sardemff7.net>
2018-07-31 19:57:55 +0200

    exec: Add StdInTemplate= to write to command’s stdin
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- plugins/exec/src/exec.c
+++ plugins/exec/src/exec.c
@@ -33,6 +33,7 @@ struct _EventdPluginContext {
 
 struct _EventdPluginAction {
     FormatString *command;
+    FormatString *stdin_template;
 };
 
 static void
@@ -40,6 +41,7 @@ _eventd_exec_action_free(gpointer data)
 {
     EventdPluginAction *action = data;
 
+    evhelpers_format_string_unref(action->stdin_template);
     evhelpers_format_string_unref(action->command);
 
     g_slice_free(EventdPluginAction, action);
@@ -75,6 +77,7 @@ _eventd_exec_action_parse(EventdPluginContext *context, GKeyFile *config_file)
 {
     gboolean disable = FALSE;
     FormatString *command = NULL;
+    FormatString *stdin_template = NULL;
 
     if ( ! g_key_file_has_group(config_file, "Exec") )
         return NULL;
@@ -88,13 +91,22 @@ _eventd_exec_action_parse(EventdPluginContext *context, GKeyFile *config_file)
     if ( evhelpers_config_key_file_get_format_string(config_file, "Exec", "Command", &command) < 0 )
         return NULL;
 
+    if ( evhelpers_config_key_file_get_template(config_file, "Exec", "StdInTemplate", &stdin_template) < 0 )
+        goto fail;
+
     EventdPluginAction *action;
     action = g_slice_new(EventdPluginAction);
     action->command = command;
+    action->stdin_template = stdin_template;
 
     context->actions = g_slist_prepend(context->actions, action);
 
     return action;
+
+fail:
+    evhelpers_format_string_unref(stdin_template);
+    evhelpers_format_string_unref(command);
+    return NULL;
 }
 
 static void
@@ -113,14 +125,51 @@ static void
 _eventd_exec_event_action(EventdPluginContext *context, EventdPluginAction *action, EventdEvent *event)
 {
     gchar *cmd;
+    gchar **argv;
+
+    gint in_, *in = ( action->stdin_template != NULL ) ? &in_ : NULL;
     GError *error = NULL;
 
     cmd = evhelpers_format_string_get_string(action->command, event, NULL, NULL);
-
-    if ( ! g_spawn_command_line_async(cmd, &error) )
+    if ( ! g_shell_parse_argv(cmd, NULL, &argv, &error) )
+    {
+        g_warning("Couldn't parse command line '%s': %s", cmd, error->message);
+        g_clear_error(&error);
+    }
+    else if ( ! g_spawn_async_with_pipes(NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, in, NULL, NULL, &error) )
     {
         g_warning("Couldn't spawn '%s': %s", cmd, error->message);
         g_clear_error(&error);
+    }
+    else if ( in != NULL )
+    {
+        GIOChannel *io;
+        gchar *data;
+
+#ifdef G_OS_UNIX
+        io = g_io_channel_unix_new(*in);
+#else /* ! G_OS_UNIX */
+        io = g_io_channel_win32_new_fd(*in);
+#endif /* ! G_OS_UNIX */
+        data = evhelpers_format_string_get_string(action->stdin_template, event, NULL, NULL);
+
+        GIOStatus status;
+        gsize bytes_written;
+        status = g_io_channel_write_chars(io, data, -1, &bytes_written, &error);
+        if ( status != G_IO_STATUS_NORMAL )
+        {
+            g_warning("Could not write to command stdin '%s': %s", cmd, error->message);
+            g_clear_error(&error);
+        }
+        status = g_io_channel_shutdown(io, TRUE, &error);
+        if ( status != G_IO_STATUS_NORMAL )
+        {
+            g_warning("Could not properly shutdown stdin of '%s': %s", cmd, error->message);
+            g_clear_error(&error);
+        }
+        g_io_channel_unref(io);
+
+        g_free(data);
     }
 
     g_free(cmd);
