5e29d5caacd6ef7a8a0d1e1741abca44cbd9a289
Quentin Glidic <sardemff7+git@sardemff7.net>
2018-08-18 15:05:12 +0200

    nd/libweston: Rename prefix to match eventd
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- plugins/nd/src/libweston-plugin.c
+++ plugins/nd/src/libweston-plugin.c
@@ -37,7 +37,7 @@
 #include <compositor.h>
 #include "notification-area-unstable-v1-server-protocol.h"
 
-struct weston_notification_area {
+typedef struct {
     struct weston_compositor *compositor;
     struct wl_resource *binding;
     struct weston_layer layer;
@@ -46,39 +46,39 @@ struct weston_notification_area {
     struct wl_listener output_created_listener;
     struct wl_listener output_destroyed_listener;
     struct wl_listener output_moved_listener;
-};
+} EventdNdWestonManager;
 
-struct weston_notification_area_notification {
+typedef struct {
     struct wl_resource *resource;
-    struct weston_notification_area *na;
+    EventdNdWestonManager *manager;
     struct weston_surface *surface;
     struct weston_view *view;
     struct wl_listener view_destroy_listener;
-};
+} EventdNdWestonNotification;
 
 static void
-_weston_notification_area_request_destroy(struct wl_client *client, struct wl_resource *resource)
+_eventd_nd_weston_request_destroy(struct wl_client *client, struct wl_resource *resource)
 {
     wl_resource_destroy(resource);
 }
 
 static struct weston_output *
-_weston_notification_area_get_default_output(struct weston_notification_area *na)
+_eventd_nd_weston_get_default_output(EventdNdWestonManager *manager)
 {
-    if ( wl_list_empty(&na->compositor->output_list) )
+    if ( wl_list_empty(&manager->compositor->output_list) )
         return NULL;
 
-    return wl_container_of(na->compositor->output_list.next, na->output, link);
+    return wl_container_of(manager->compositor->output_list.next, manager->output, link);
 }
 
 static void
-_weston_notification_area_notification_request_move(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y)
+_eventd_nd_weston_notification_request_move(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y)
 {
-    struct weston_notification_area_notification *self = wl_resource_get_user_data(resource);
+    EventdNdWestonNotification *self = wl_resource_get_user_data(resource);
     int32_t dx, dy;
 
-    x += self->na->workarea.x;
-    y += self->na->workarea.y;
+    x += self->manager->workarea.x;
+    y += self->manager->workarea.y;
     dx = self->view->geometry.x - x;
     dy = self->view->geometry.y - y;
 
@@ -88,7 +88,7 @@ _weston_notification_area_notification_request_move(struct wl_client *client, st
 
     if ( ! weston_view_is_mapped(self->view) )
     {
-        weston_layer_entry_insert(&self->na->layer.view_list, &self->view->layer_link);
+        weston_layer_entry_insert(&self->manager->layer.view_list, &self->view->layer_link);
         self->view->is_mapped = true;
         weston_fade_run(self->view, 0, 1, 400.0, NULL, NULL);
     }
@@ -97,55 +97,55 @@ _weston_notification_area_notification_request_move(struct wl_client *client, st
 }
 
 static void
-_weston_notification_area_notification_view_destroyed(struct wl_listener *listener, void *data)
+_eventd_nd_weston_notification_view_destroyed(struct wl_listener *listener, void *data)
 {
-    struct weston_notification_area_notification *self = wl_container_of(listener, self, view_destroy_listener);
+    EventdNdWestonNotification *self = wl_container_of(listener, self, view_destroy_listener);
 
     wl_list_remove(&self->view_destroy_listener.link);
     weston_view_damage_below(self->view);
     self->view = NULL;
 }
 
-static const struct zww_notification_v1_interface weston_notification_area_notification_implementation = {
-    .destroy = _weston_notification_area_request_destroy,
-    .move = _weston_notification_area_notification_request_move,
+static const struct zww_notification_v1_interface _eventd_nd_weston_notification_implementation = {
+    .destroy = _eventd_nd_weston_request_destroy,
+    .move = _eventd_nd_weston_notification_request_move,
 };
 
 static void
-_weston_notification_area_notification_fade_out_done(struct weston_view_animation *animation, void *data)
+_eventd_nd_weston_notification_fade_out_done(struct weston_view_animation *animation, void *data)
 {
-    struct weston_notification_area_notification *self = data;
+    EventdNdWestonNotification *self = data;
 
     weston_surface_destroy(self->surface);
     free(self);
 }
 
 static void
-_weston_notification_area_notification_destroy(struct wl_resource *resource)
+_eventd_nd_weston_notification_destroy(struct wl_resource *resource)
 {
-    struct weston_notification_area_notification *self = wl_resource_get_user_data(resource);
+    EventdNdWestonNotification *self = wl_resource_get_user_data(resource);
 
-    weston_fade_run(self->view, 1, 0, 400.0, _weston_notification_area_notification_fade_out_done, self);
+    weston_fade_run(self->view, 1, 0, 400.0, _eventd_nd_weston_notification_fade_out_done, self);
 }
 
 static void
-_weston_notification_area_create_notification(struct wl_client *client, struct wl_resource *resource, uint32_t id, struct wl_resource *surface_resource)
+_eventd_nd_weston_create_notification(struct wl_client *client, struct wl_resource *resource, uint32_t id, struct wl_resource *surface_resource)
 {
-    struct weston_notification_area *na = wl_resource_get_user_data(resource);
+    EventdNdWestonManager *manager = wl_resource_get_user_data(resource);
     struct weston_surface *surface = wl_resource_get_user_data(surface_resource);
 
     if (weston_surface_set_role(surface, "ww_notification", resource, ZWW_NOTIFICATION_AREA_V1_ERROR_ROLE) < 0)
         return;
 
-    struct weston_notification_area_notification *self;
-    self = zalloc(sizeof(struct weston_notification_area_notification));
+    EventdNdWestonNotification *self;
+    self = zalloc(sizeof(EventdNdWestonNotification));
     if ( self == NULL )
     {
         wl_resource_post_no_memory(surface_resource);
         return;
     }
 
-    self->na = na;
+    self->manager = manager;
     self->surface = surface;
 
     self->view = weston_view_create(self->surface);
@@ -166,125 +166,125 @@ _weston_notification_area_create_notification(struct wl_client *client, struct w
     }
 
     ++self->surface->ref_count;
-    self->view_destroy_listener.notify = _weston_notification_area_notification_view_destroyed;
+    self->view_destroy_listener.notify = _eventd_nd_weston_notification_view_destroyed;
     wl_signal_add(&self->view->destroy_signal, &self->view_destroy_listener);
-    wl_resource_set_implementation(self->resource, &weston_notification_area_notification_implementation, self, _weston_notification_area_notification_destroy);
+    wl_resource_set_implementation(self->resource, &_eventd_nd_weston_notification_implementation, self, _eventd_nd_weston_notification_destroy);
 }
 
-static const struct zww_notification_area_v1_interface weston_notification_area_implementation = {
-    .destroy = _weston_notification_area_request_destroy,
-    .create_notification = _weston_notification_area_create_notification,
+static const struct zww_notification_area_v1_interface _eventd_nd_weston_manager_implementation = {
+    .destroy = _eventd_nd_weston_request_destroy,
+    .create_notification = _eventd_nd_weston_create_notification,
 };
 
 static void
-_weston_notification_area_set_output(struct weston_notification_area *na, struct weston_output *output)
+_eventd_nd_weston_set_output(EventdNdWestonManager *manager, struct weston_output *output)
 {
     struct weston_geometry workarea = { 0, 0, 0, 0 };
-    na->output = output;
-    if ( na->output != NULL )
+    manager->output = output;
+    if ( manager->output != NULL )
     {
         /* TODO: Support dock area API */
         {
-            workarea.x = na->output->x;
-            workarea.y = na->output->y;
-            workarea.width = na->output->width;
-            workarea.height = na->output->height;
+            workarea.x = manager->output->x;
+            workarea.y = manager->output->y;
+            workarea.width = manager->output->width;
+            workarea.height = manager->output->height;
         }
     }
 
-    if ( ( na->workarea.x == workarea.x ) && ( na->workarea.y == workarea.y ) && ( na->workarea.width == workarea.width ) && ( na->workarea.height == workarea.height ) )
+    if ( ( manager->workarea.x == workarea.x ) && ( manager->workarea.y == workarea.y ) && ( manager->workarea.width == workarea.width ) && ( manager->workarea.height == workarea.height ) )
         return;
 
-    na->workarea = workarea;
+    manager->workarea = workarea;
 
-    if ( na->binding != NULL )
-        zww_notification_area_v1_send_geometry(na->binding, na->workarea.width, na->workarea.height, na->output->current_scale);
+    if ( manager->binding != NULL )
+        zww_notification_area_v1_send_geometry(manager->binding, manager->workarea.width, manager->workarea.height, manager->output->current_scale);
 }
 
 static void
-_weston_notification_area_unbind(struct wl_resource *resource)
+_eventd_nd_weston_manager_unbind(struct wl_resource *resource)
 {
-    struct weston_notification_area *na = wl_resource_get_user_data(resource);
+    EventdNdWestonManager *manager = wl_resource_get_user_data(resource);
 
-    na->binding = NULL;
+    manager->binding = NULL;
 }
 
 static void
-_weston_notification_area_bind(struct wl_client *client, void *data, uint32_t version, uint32_t id)
+_eventd_nd_weston_manager_bind(struct wl_client *client, void *data, uint32_t version, uint32_t id)
 {
-    struct weston_notification_area *na = data;
+    EventdNdWestonManager *manager = data;
     struct wl_resource *resource;
 
     resource = wl_resource_create(client, &zww_notification_area_v1_interface, version, id);
-    wl_resource_set_implementation(resource, &weston_notification_area_implementation, na, _weston_notification_area_unbind);
+    wl_resource_set_implementation(resource, &_eventd_nd_weston_manager_implementation, manager, _eventd_nd_weston_manager_unbind);
 
-    if ( na->binding != NULL )
+    if ( manager->binding != NULL )
     {
         wl_resource_post_error(resource, ZWW_NOTIFICATION_AREA_V1_ERROR_BOUND, "interface object already bound");
         wl_resource_destroy(resource);
         return;
     }
 
-    na->binding = resource;
+    manager->binding = resource;
 
-    if ( na->output == NULL )
-        _weston_notification_area_set_output(na, _weston_notification_area_get_default_output(na));
+    if ( manager->output == NULL )
+        _eventd_nd_weston_set_output(manager, _eventd_nd_weston_get_default_output(manager));
     else
-        zww_notification_area_v1_send_geometry(na->binding, na->workarea.width, na->workarea.height, na->output->current_scale);
+        zww_notification_area_v1_send_geometry(manager->binding, manager->workarea.width, manager->workarea.height, manager->output->current_scale);
 }
 
 static void
-_weston_notification_area_output_created(struct wl_listener *listener, void *data)
+_eventd_nd_weston_manager_output_created(struct wl_listener *listener, void *data)
 {
-    struct weston_notification_area *na = wl_container_of(listener, na, output_created_listener);
+    EventdNdWestonManager *manager = wl_container_of(listener, manager, output_created_listener);
 
-    if ( na->output == NULL )
-        _weston_notification_area_set_output(na, _weston_notification_area_get_default_output(na));
+    if ( manager->output == NULL )
+        _eventd_nd_weston_set_output(manager, _eventd_nd_weston_get_default_output(manager));
 }
 
 static void
-_weston_notification_area_output_destroyed(struct wl_listener *listener, void *data)
+_eventd_nd_weston_manager_output_destroyed(struct wl_listener *listener, void *data)
 {
-    struct weston_notification_area *na = wl_container_of(listener, na, output_destroyed_listener);
+    EventdNdWestonManager *manager = wl_container_of(listener, manager, output_destroyed_listener);
     struct weston_output *output = data;
 
-    if ( na->output == output )
-        _weston_notification_area_set_output(na, _weston_notification_area_get_default_output(na));
+    if ( manager->output == output )
+        _eventd_nd_weston_set_output(manager, _eventd_nd_weston_get_default_output(manager));
 }
 
 static void
-_weston_notification_area_output_moved(struct wl_listener *listener, void *data)
+_eventd_nd_weston_manager_output_moved(struct wl_listener *listener, void *data)
 {
-    struct weston_notification_area *na = wl_container_of(listener, na, output_moved_listener);
+    EventdNdWestonManager *manager = wl_container_of(listener, manager, output_moved_listener);
     struct weston_output *output = data;
 
-    if ( na->output == output )
-        _weston_notification_area_set_output(na, na->output);
+    if ( manager->output == output )
+        _eventd_nd_weston_set_output(manager, manager->output);
 }
 
 EVENTD_EXPORT int
 wet_module_init(struct weston_compositor *compositor, int *argc, char *argv[])
 {
-    struct weston_notification_area *na;
+    EventdNdWestonManager *manager;
 
-    na = zalloc(sizeof(struct weston_notification_area));
-    if ( na == NULL )
+    manager = zalloc(sizeof(EventdNdWestonManager));
+    if ( manager == NULL )
         return -1;
 
-    na->compositor = compositor;
+    manager->compositor = compositor;
 
-    na->output_created_listener.notify = _weston_notification_area_output_created;
-    na->output_destroyed_listener.notify = _weston_notification_area_output_destroyed;
-    na->output_moved_listener.notify = _weston_notification_area_output_moved;
-    wl_signal_add(&na->compositor->output_created_signal, &na->output_created_listener);
-    wl_signal_add(&na->compositor->output_destroyed_signal, &na->output_destroyed_listener);
-    wl_signal_add(&na->compositor->output_moved_signal, &na->output_moved_listener);
+    manager->output_created_listener.notify = _eventd_nd_weston_manager_output_created;
+    manager->output_destroyed_listener.notify = _eventd_nd_weston_manager_output_destroyed;
+    manager->output_moved_listener.notify = _eventd_nd_weston_manager_output_moved;
+    wl_signal_add(&manager->compositor->output_created_signal, &manager->output_created_listener);
+    wl_signal_add(&manager->compositor->output_destroyed_signal, &manager->output_destroyed_listener);
+    wl_signal_add(&manager->compositor->output_moved_signal, &manager->output_moved_listener);
 
-    if ( wl_global_create(na->compositor->wl_display, &zww_notification_area_v1_interface, 1, na, _weston_notification_area_bind) == NULL)
+    if ( wl_global_create(manager->compositor->wl_display, &zww_notification_area_v1_interface, 1, manager, _eventd_nd_weston_manager_bind) == NULL)
         return -1;
 
-    weston_layer_init(&na->layer, na->compositor);
-    weston_layer_set_position(&na->layer, WESTON_LAYER_POSITION_UI);
+    weston_layer_init(&manager->layer, manager->compositor);
+    weston_layer_set_position(&manager->layer, WESTON_LAYER_POSITION_UI);
 
     return 0;
 }
